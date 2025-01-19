f9a8f73903f771b6ac141c049fe780ec53917d7b
matveev96 <sasha.matveev1996@icloud.com>
2025-01-19 16:24:43 +0300

    add to App optimized components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react';
+import {Task_3_3} from "./task_3/Task_3.3";
+import {SlowComponent} from "./task_3/slowComponent/SlowComponent";
+import {Task_3_2} from "./task_3/Task_3.2";
+import {Task_3_1} from "./task_3/Task_3.1";
+import {Task_3_4} from "./task_3/Task_3.4";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export type UserType = {
+    [key: string]: { name: string, id: string }
+};
 
-export default App;
+const DATA_7: UserType[] = [
+    {u1: {name: 'Gleb', id: '1'}},
+    {u2: {name: 'Dima', id: '2'}},
+    {u3: {name: 'Svetlana', id: '3'}},
+    {u4: {name: 'Artur', id: '4'}},
+    {u5: {name: 'Vera', id: '5'}},
+    {u6: {name: 'Vlad', id: '6'}},
+];
+
+export const App = () => {
+    return (
+        <Fragment>
+            <Task_3_1/>
+            <Task_3_2/>
+            <Task_3_3>
+                <SlowComponent/>
+            </Task_3_3>
+            <Task_3_4/>
+            <hr/>
+        </Fragment>
+    );
+};
