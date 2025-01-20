f86a33c3ebf6968bb9d121358e850388a0c54019
matveev96 <sasha.matveev1996@icloud.com>
2025-01-20 14:29:35 +0300

    add first Task of Dop lesson

---
+++ dop_task_1/src/task_1/Task_1.tsx
@@ -0,0 +1,34 @@
+import {ChangeEvent, FC, memo, useCallback, useState} from 'react';
+
+const CONTAINER_STYLES = {display: 'flex', flexDirection: 'column', gap: 10};
+
+type PropsType = { value: string, onChange: (e: ChangeEvent<HTMLInputElement>) => void };
+
+// Task 1
+// If change value on input Title component not re-render
+// If change value on input Task_1 component not re-render
+
+export const Task_1 = () => {
+    console.log('Task_1')
+    return (
+        <div style={{...CONTAINER_STYLES} as any}>
+            <Input/>
+            <Title title="I am a title"/>
+        </div>
+    );
+};
+
+const Title =  (props: { title: string }) => {
+    console.log('Title')
+    return <h3>{props.title}</h3>;
+}
+
+const Input = () => {
+    const [value, setValue] = useState('');
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.currentTarget.value);
+    };
+    return (
+        <input type="text" placeholder="Placeholder" value={value} onChange={handleChange}/>
+    );
+};
